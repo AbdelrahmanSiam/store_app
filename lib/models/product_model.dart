e84efddcfeff0943a5e3048741b782fc89e6b211
Abdulrahman <syamabdo382@gmail.com>
2025-10-20 02:43:05 +0300

    create two models for a product

---
+++ lib/models/product_model.dart
@@ -0,0 +1,31 @@
+import 'package:store_app/models/rating_model.dart';
+
+class ProductModel {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final String category;
+  final String image;
+  final RatingModel rating;
+
+  ProductModel(
+      {required this.id,
+      required this.title,
+      required this.price,
+      required this.description,
+      required this.category,
+      required this.image,
+      required this.rating,});
+
+  factory ProductModel.fromjson(json) {
+    return ProductModel(
+        id: json['id'],
+        title: json['title'],
+        price: json['price'],
+        description: json['description'],
+        category: json['category'],
+        image: json['image'],
+        rating: RatingModel.fromjson(json['rating']));
+  }
+}
