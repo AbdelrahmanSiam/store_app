d598b2366f5911678d6908443e0c8d4d57d42360
Abdulrahman <syamabdo382@gmail.com>
2025-10-20 03:11:25 +0300

    finsh getAllProductService metho

---
+++ lib/services/get_all_product_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http; // using allias (as) to can use it
+import 'package:store_app/models/product_model.dart';
+
+class GetAllProductService {
+  // This function will return all products from API it as List with maps but we will convert these maps to models 
+  Future<List<ProductModel>> getAllProduct() async {
+    http.Response response =
+        await http.get(Uri.parse("https://fakestoreapi.com/products"));
+
+    List<dynamic> data = jsonDecode(
+        response.body); // we need to convert it to list of model into next step
+    List<ProductModel> productList = []; // new list (list of models)
+
+    // we will make for loop to make this convert
+    for (int i = 0; i < data.length; i++) {
+      productList.add(ProductModel.fromjson(data[i]));
+    }
+
+    return productList;
+  }
+}
