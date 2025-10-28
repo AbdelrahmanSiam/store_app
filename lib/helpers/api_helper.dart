078163a4867d57506d387ec66486882a6e82b18d
Abdulrahman <syamabdo382@gmail.com>
2025-10-28 11:46:39 +0300

    add put method at ApiHelper class

--- lib/helpers/api_helper.dart
+++ lib/helpers/api_helper.dart
@@ -1,16 +1,52 @@
 import 'dart:convert';
-
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class ApiHelper {
+  // public get function
   Future<dynamic> get({required String url}) async {
     http.Response response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       return jsonDecode(response.body);
+    } else {
+      throw Exception(
+          "There is a status code exception ${response.statusCode}");
+    }
+  }
+  // public post method
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({'Authorization': 'Barear $token'});
+    }
+    http.Response response =
+        await http.post(Uri.parse(url), body: body, headers: headers);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception(
+          "There is problem at status Code ${response.statusCode} with body ${jsonDecode(response.body)}");
+    }
+  }
+
+  // public put method
+  Future<dynamic> put({required String url,@required dynamic body , @required Stirng? token})async{
+    Map<String,dynamic> headers = {};
+    headers.addAll('Content-Type':'application/x-www-form-urlencoded');
+    if(token !=null){
+      headers.addAll('Barer $token');
+    }
+    http.Response response = await http.put(Uri.parse(url), body:body , headers : headers);
+    if(response.statusCode == 200){
+      return jsonDecode(response.body);
     }
     else{
       throw Exception(
-          "There is a status code exception ${response.statusCode}");
+          "There is problem at status Code ${response.statusCode} with body ${jsonDecode(response.body)}");
     }
   }
 }
--- lib/services/get_category.dart
+++ lib/services/get_category.dart
@@ -6,10 +6,7 @@ import 'package:store_app/models/product_model.dart';
 class GetCategory {
   // the result of category name may be more than one model (list)
   Future<List<ProductModel>> getCategory({required String categoryName}) async {
-
-    String url = "https://fakestoreapi.com/products/category/$categoryName";
-    
-   List<dynamic> data = await ApiHelper().get(url: url);
+   List<dynamic> data = await ApiHelper().get(url: "https://fakestoreapi.com/products/category/$categoryName");
 
       List<ProductModel> list = [];
       for (int i = 0; i < data.length; i++) {
